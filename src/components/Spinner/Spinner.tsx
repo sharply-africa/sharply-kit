ed146ca7a231a169d25264f95c63ad0d03a5280e
Remilekun Salami <remilekun.salami@gmail.com>
2022-06-01 19:26:21 +0100

    feat: spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef } from 'react';
+import { keyframes } from '@emotion/react';
+import { Box, BoxProps } from '~src/components/Box';
+
+const spin = keyframes({
+  '0%': {
+    transform: 'rotate(0deg)',
+  },
+  '100%': {
+    transform: 'rotate(360deg)',
+  },
+});
+
+interface SpinnerProps extends BoxProps {
+  color: string;
+  emptyColor: string;
+  size: string | number;
+  speed: string | number;
+  styles: React.CSSProperties;
+  thickness: string;
+}
+
+const Spinner = forwardRef((props: SpinnerProps, ref) => {
+  const {
+    color,
+    emptyColor = 'transparent',
+    size = '2.5rem',
+    speed = '0.45s',
+    styles,
+    thickness = '2px',
+    ...rest
+  } = props;
+
+  const spinnerStyles = {
+    animation: `${spin} ${speed} linear infinite`,
+    borderBottomColor: emptyColor,
+    borderColor: 'currentColor',
+    borderLeftColor: emptyColor,
+    borderRadius: 'full',
+    borderStyle: 'solid',
+    borderWidth: thickness,
+    color,
+    display: 'inline-block',
+    height: size,
+    width: size,
+    ...styles,
+  };
+
+  return <Box ref={ref} __css={spinnerStyles} {...rest}></Box>;
+});
+
+export default Spinner;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ThemeProvider from '~src/context/ThemeContext';
 import { Box } from './components/Box';
 import { Text } from './components/Text';
+import { Spinner } from './components/Spinner';
 
-export { ThemeProvider, Box, Text };
+export { ThemeProvider, Box, Text, Spinner };
