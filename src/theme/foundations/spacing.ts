d1c2195a9e7c446a661be8016519b5b0602c7444
Remilekun Salami <remilekun.salami@gmail.com>
2022-12-19 13:51:56 +0100

    fix: spacing keys

--- src/theme/foundations/spacing.ts
+++ src/theme/foundations/spacing.ts
@@ -3,6 +3,8 @@ export const spacing = [...new Array(96)].reduce((acc, _, x) => {
     ...acc,
     [x]: `${(x * 4) / 10}rem`,
     [`-${x}`]: `${-(x * 4) / 10}rem`,
+    [x / 2]: `${(x * 2) / 10}rem`,
+    [`-${x / 2}`]: `${-(x * 2) / 10}rem`,
   };
   return acc;
 }, {});
