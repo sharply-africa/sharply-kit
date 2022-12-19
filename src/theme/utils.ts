d8ab946d24b4348157f10571fe734ee0ff16ffdc
Remilekun Salami <remilekun.salami@gmail.com>
2022-12-19 14:47:51 +0100

    fix: extendTheme function

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -1,5 +1,27 @@
 import defaltTheme from './theme';
 
-export const extendTheme = (theme) => {
-  return { ...defaltTheme, ...theme };
+export const extendTheme = (newTheme, oldTheme = defaltTheme) => {
+  const mergedTheme = {};
+
+  for (const key in oldTheme) {
+    if (oldTheme.hasOwnProperty(key)) {
+      if (typeof oldTheme[key] === 'object' && oldTheme[key] !== null) {
+        mergedTheme[key] = extendTheme(oldTheme[key], newTheme[key]);
+      } else {
+        mergedTheme[key] = oldTheme[key];
+      }
+    }
+  }
+
+  for (const key in newTheme) {
+    if (newTheme.hasOwnProperty(key)) {
+      if (typeof newTheme[key] === 'object' && newTheme[key] !== null) {
+        mergedTheme[key] = extendTheme(oldTheme[key], newTheme[key]);
+      } else {
+        mergedTheme[key] = newTheme[key];
+      }
+    }
+  }
+
+  return mergedTheme;
 };
